7f6526fe24a536b8a8fc0ed29a7eb8ca2bd4ddc3
toniprats07 <119038749+toniprats07@users.noreply.github.com>
2023-06-08 12:37:09 +0200

    Ejercicio 3

--- sprint6/sprint6/src/components/escena/escena.tsx
+++ sprint6/sprint6/src/components/escena/escena.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled from "styled-components";
 
 interface EscenaProps {
     texto: string;
   }
+
+const StyledEscena = styled.p`
+    font-size: 18px;
+    text-align: center;
+    border-radius: 30px;
+    border: 2px solid black;
+    color: black;
+    padding: 1em;
+    margin: 20px;
+`;
   
   const Escena: React.FC<EscenaProps> = ({texto}) => {
-    return <p>{texto}</p>;
+    return <StyledEscena>{texto}</StyledEscena>;
   };
   
   export default Escena;
