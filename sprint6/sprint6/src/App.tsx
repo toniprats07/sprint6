062e76e0c2abac5f78e44f6c69291fe798d6392e
toniprats07 <119038749+toniprats07@users.noreply.github.com>
2023-06-08 10:16:47 +0200

    Ejercicio 1

---
+++ sprint6/sprint6/src/App.tsx
@@ -0,0 +1,13 @@
+import Escena from './components/escena/escena';
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    <Escena/>
+  </div>
+  )
+}
+
+export default App
---
+++ sprint6/sprint6/src/components/escena/escena.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let Escena: React.FC = () => {
+  return (
+    <p>
+      El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial
+    </p>
+  );
+};
+
+export default Escena;
